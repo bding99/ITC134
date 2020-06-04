711794743121ca21e58e936edbb19f76857f3a9b
bding99 <66132765+bding99@users.noreply.github.com>
2020-06-04 16:29:21 -0700

    indent for the Print function

--- selectionsort.py
+++ selectionsort.py
@@ -4,13 +4,17 @@ array = [12,36,,4,6,5,2,8,9]
 #function for selection sort using the above array
 def selectionSort(array)
 	
+	#variable called n for length of the array
 	n = len(array)
 
+	#for loop 
 	for i in range(n)	#Use the length of the array to determine how many times you are going to run the loop
 
 		#Initially assume the first element of the unsorted part as the minimum
 		minimum = i
 
+		#nested for loop 
+		#
 		for j in range(i*1,n):
 
 			if (array[j] < array[minimum]):
