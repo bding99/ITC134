f1a3bddc0b825601577cf55ad8bbd2feca720953
bding99 <66132765+bding99@users.noreply.github.com>
2020-06-05 09:02:12 -0700

    Hacking Tool

---
+++ BluezHack.py
@@ -0,0 +1,22 @@
+# This script that advertises a bluetooth low energer beacon for 15 seconds
+#import class library time
+import time
+
+from bluetooth.ble import BeaconService  #Import a 3rd party control module
+
+#Create an instance of the object from the 3rd party class
+#Create variable for BeaconService function
+service = BeaconService()
+
+
+service.start_advertising("11111111-2222-3333-4444-555555555555",1,1,1,200)
+
+#bluetooth scrub
+#looking for the bluetooth id
+time.sleep(15)
+#event stop time after 15 seconds
+
+service.stop_advertising()
+#event to stop advertising for service 
+print("Done.")
+#print displays the string Done
