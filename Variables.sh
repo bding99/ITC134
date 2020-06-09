cf1da24f153d73ae24e9d5ce079c3ae10a5d1fc3
bding99 <66132765+bding99@users.noreply.github.com>
2020-06-09 09:01:34 -0700

    Variables

---
+++ Variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+greeting = "Welcome" 
+
+user=$(whoami)
+
+day=$(date +%A)
+
+echo "$greeting bacj $user! Today is $day, which is the best day of the entire week"
+echo "Your Bash shell  version is : $BASH_VERSION. Cool!"
+
